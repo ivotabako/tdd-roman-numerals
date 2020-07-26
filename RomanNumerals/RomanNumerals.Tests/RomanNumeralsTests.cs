bfe584d59aace6a940a3480e730e82e2d8cd18e6
Ivaylo Tabakov <ivo_tabako@yahoo.com>
2020-07-26 15:28:32 +0200

    done

--- RomanNumerals/RomanNumerals.Tests/RomanNumeralsTests.cs
+++ RomanNumerals/RomanNumerals.Tests/RomanNumeralsTests.cs
@@ -12,6 +12,8 @@ namespace RomanNumerals.Tests
             RomanNumerals.FromInteger(2).Should().Be("II");
             RomanNumerals.FromInteger(3).Should().Be("III");
             RomanNumerals.FromInteger(4).Should().Be("IV");
+            RomanNumerals.FromInteger(5).Should().Be("V");
+            RomanNumerals.FromInteger(1981).Should().Be("MCMLXXXI");
         }
     }
 }
--- RomanNumerals/RomanNumerals/RomanNumerals.cs
+++ RomanNumerals/RomanNumerals/RomanNumerals.cs
@@ -6,6 +6,17 @@ namespace RomanNumerals
     {
         private static Dictionary<int, string> _mapping = new Dictionary<int, string>()
         {
+            { 1000, "M"},
+            { 900, "CM"},
+            { 500, "D"},
+            { 400, "CD"},
+            { 100, "C"},
+            { 90, "XC"},
+            { 50, "L"},
+            { 40, "XL"},
+            { 10, "X"},
+            { 9, "IX"},
+            { 5, "V"},
             {4, "IV"},
             {1, "I"}
         };
@@ -23,17 +34,7 @@ namespace RomanNumerals
             }
 
             return res;
-            //if (number == 4)
-            //    return "IV";
 
-            //var res = "";
-            //while (number > 0)
-            //{
-            //    res += "I";
-            //    number--;
-            //}
-
-            //return res;
         }
     }
 }
